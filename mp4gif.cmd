33ed80fdc9d9e3f5596cdbc66536a2b27bf3de3d
Alexander Andreev <andreev-alef@users.noreply.github.com>
2021-12-29 12:10:51 +0300

    Экранирование кавычек для входного файла

--- mp4gif.cmd
+++ mp4gif.cmd
@@ -2,6 +2,6 @@ for %%z in (*.mp4) do (
     set palette="palette.png"
     rem set filters="fps=20,scale=360:-1:flags=lanczos"
     set filters="fps=20,scale=480:-1:flags=lanczos"
-    ffmpeg -i "%%z" -vf ^"%filters%,palettegen^" -y %palette%
-    ffmpeg -i "%%z" -i %palette% -lavfi ^"%filters% [x]; [x][1:v] paletteuse^" -y "%%~nz.gif"
+    ffmpeg -i ^"%%z^" -vf ^"%filters%,palettegen^" -y %palette%
+    ffmpeg -i ^"%%z^" -i %palette% -lavfi ^"%filters% [x]; [x][1:v] paletteuse^" -y "%%~nz.gif"
 )
