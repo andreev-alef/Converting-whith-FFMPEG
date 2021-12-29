f68bbe2075f353d2433c6111ce4328d191809442
Alexander Andreev <andreev-alef@users.noreply.github.com>
2021-12-29 10:45:35 +0300

    Convert all mp4-files in folder to GIF
    Convert all mp4-files in folder to GIF-animated image

---
+++ mp4gif.cmd
@@ -0,0 +1,7 @@
+for %%z in (*.mp4) do ( 
+    set palette="palette.png"
+    rem set filters="fps=20,scale=360:-1:flags=lanczos"
+    set filters="fps=20,scale=480:-1:flags=lanczos"
+    ffmpeg -i "%%z" -vf ^"%filters%,palettegen^" -y %palette%
+    ffmpeg -i "%%z" -i %palette% -lavfi ^"%filters% [x]; [x][1:v] paletteuse^" -y "%%~nz.gif"
+)
